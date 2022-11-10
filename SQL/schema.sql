1b2229125a48c4bdbbfcc06ebed9be3f41b5969c
Amanda <amanda@Amandas-MacBook-Air-5.local>
2022-11-09 20:15:25 -0800

    complete_2021 connected to SQL

--- SQL/schema.sql
+++ SQL/schema.sql
@@ -1,3 +1,9 @@
+--Database stores static data for use during the project- (So far have connected States, 2020, and 2021. Waiting on 2019 data)
+--Database interfaces with the project in some format (connects to the model)- Erica
+--Includes at least one join using the database language (not including pandas) -Create DB that has top 10 wine sales by state joined with the information from the states table?
+--Includes at least one connection string (using SQALchemy or PyMongo) - DONE (I think) (Connected through pandas/JN)
+--Provide ERD with relationships- DONE
+
 --2019 has not been imported yet, waiting on data to be provided and cleaned. 
 CREATE TABLE complete_2019 (
 	 "Order_Number" INT NOT NULL,
@@ -64,12 +70,17 @@ DROP TABLE "State Research" CASCADE;
 		
 --SELECT COUNT(*) FROM complete_2019;
 SELECT COUNT(*) FROM complete_2020;
---SELECT COUNT(*) FROM complete_2021;
+SELECT COUNT(*) FROM complete_2021;
 SELECT COUNT (*) FROM "State Research";
 
---confirm column names are correct and data imported correctly.
+--confirm column names are correct and data imported correctly from 2020.
 SELECT * FROM complete_2020
 LIMIT 10;
 
 --Also confirmed by right clicking on "complete_2020" table in the left panel>properties>columns.
 --can edit PK and FK from the above directions as well.
+
+--confirm column names are correct and data imported correctly from 2021.
+SELECT * FROM complete_2021
+LIMIT 10;
+
