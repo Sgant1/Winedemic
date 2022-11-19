9be17d463ac88782587f6c8ba0e7fe30517559e4
Amanda <amanda@Amandas-MacBook-Air-5.local>
2022-11-19 13:51:57 -0800

    connecting Winedemic Complete to pgAdmin

--- SQL/schema.sql
+++ SQL/schema.sql
@@ -1,9 +1,9 @@
 <<<<<<< HEAD
 =======
---Database stores static data for use during the project- (So far have connected States, 2020, and 2021. Waiting on 2019 data)
+--Database stores static data for use during the project- (So far have connected States, 2019, 2020, and 2021.)
 --Database interfaces with the project in some format (connects to the model)- Erica
---Includes at least one join using the database language (not including pandas) -Create DB that has top 10 wine sales by state joined with the information from the states table?
---Includes at least one connection string (using SQALchemy or PyMongo) - DONE (I think) (Connected through pandas/JN)
+--Includes at least one join using the database language (not including pandas) -Create DB joining all three years and States.
+--Includes at least one connection string (using SQALchemy or PyMongo) - (Connected via pandas in Jupyter Notebook)
 --Provide ERD with relationships- DONE
 
 CREATE TABLE "State Research" (
@@ -18,7 +18,7 @@ CREATE TABLE "State Research" (
 -- DROP TABLE "State Research" CASCADE;
 
 >>>>>>> main
---2019 has not been imported yet, waiting on data to be provided and cleaned. 
+--2019 has been imported and confirmed. 
 CREATE TABLE complete_2019 (
 	 "Order_Number" INT NOT NULL,
      Company_Name VARCHAR(40) NOT NULL,
@@ -77,22 +77,21 @@ SELECT COUNT(*) FROM complete_2020;
 SELECT COUNT(*) FROM complete_2021;
 SELECT COUNT (*) FROM "State Research";
 
---confirm column names are correct and data imported correctly from 2020.
+--confirm column names are correct and data imported correctly from 2019,  2020, 2021 and State Research.
+SELECT * FROM complete_2019
+LIMIT 5;
+
 SELECT * FROM complete_2020
 LIMIT 5;
 
-SELECT * FROM complete_2019
+SELECT * FROM complete_2021
 LIMIT 5;
+
 SELECT * FROM "State Research"
 LIMIT 5;
 
---Also confirmed by right clicking on "complete_2020" table in the left panel>properties>columns.
+--Also confirmed by right clicking on tables in the left panel>properties>columns.
 --can edit PK and FK from the above directions as well.
-
---confirm column names are correct and data imported correctly from 2021.
-SELECT * FROM complete_2021
-LIMIT 10;
-
 
 -- Create new table for State that excludes population
 SELECT "State", "lockdown_start", "initial_expected_lockdown_end", "Phase_1_Re-Opening_Start", "political_alignment"
@@ -118,3 +117,21 @@ ON J."State" = c."State";
 	 
 SELECT * FROM Joined_State_2020
 LIMIT 5;
+
+--Create Winedemic_Complete table
+--Imported Winedemic Complete from Pandas connection.
+--CREATE TABLE Winedemic_Complete (
+--	 "Order_Number" INT NOT NULL,
+--    Company_Name VARCHAR(40) NOT NULL,
+--	 "State" VARCHAR (40) NOT NULL,
+--	 City VARCHAR (40) NOT NULL,
+--	 Created_Date VARCHAR (40) NOT NULL,
+--	 "Item/Bottle_Count" VARCHAR (40) NOT NULL,
+--	 Weight VARCHAR (40) NOT NULL,
+--    PRIMARY KEY ("Order_Number"),
+--     FOREIGN KEY ("State") REFERENCES "State Research" ("State")
+--);
+
+--DROP TABLE Winedemic_Complete;
+SELECT * FROM public."Winedemic Complete"
+LIMIT 100
